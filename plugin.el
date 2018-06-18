6702fe7a7952b132a12e9620e6af850a7bd3c6a7
Beyang Liu <beyang@sourcegraph.com>
2018-06-18 15:49:27 -0700

    add sourcegraph-search

--- plugin.el
+++ plugin.el
@@ -1,7 +1,13 @@
 (defun sourcegraph-open ()
   (interactive)
   (message (buffer-file-name))
-  (start-process-shell-command "sgopen" "sgopen"
-                               (format "sg %s" (buffer-file-name)))
+  (start-process-shell-command "sourcegraph-open" "sourcegraph-open"
+                               (format "sg open %s" (buffer-file-name)))
   )
 
+(defun sourcegraph-search (query)
+  (interactive "squery: ")
+  (message (buffer-file-name))
+  (start-process-shell-command "sourcegraph-search" "sourcegraph-search"
+                               (format "sg search %s" query))
+  )
