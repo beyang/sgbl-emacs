282446158da488ce23a9ba1cde4952a96146cbae
Beyang Liu <beyang@sourcegraph.com>
2018-06-17 17:09:24 -0700

    .

--- plugin.el
+++ plugin.el
@@ -1,6 +1,7 @@
-(defun sourcegraph-search ()
+(defun sourcegraph-open ()
   (interactive)
   (message (buffer-file-name))
   (start-process-shell-command "sgopen" "sgopen"
                                (format "sg %s" (buffer-file-name)))
   )
+
